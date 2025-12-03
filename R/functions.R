686b772f9ab11fc84034b2ace165f099454c60f5
Agnete Troen Lundgaard <agnete.troen.lundgaard@regionh.dk>
2025-12-03 15:00:59 +0100

    Added cleaning step of lipidomics data

--- R/functions.R
+++ R/functions.R
@@ -8,11 +8,17 @@
 #' @return A tibble
 create_table_descriptive_stats <- function(data) {
   data %>%
-    dplyr::summarise(mean = mean(value), sd = sd(value), .by = metabolite) %>%
-    dplyr::mutate(dplyr::across(c(mean, sd), ~ round(., 1)),
-      mean_sd = glue::glue("{mean} ({sd})")
-    ) %>%
-    dplyr::select(Metabolite = metabolite, `mean (SD)` = mean_sd)
+    dplyr::summarise(dplyr::across(value,
+                            list(mean = mean, sd = sd,
+                                 median = median, iqr = IQR)),
+                            .by = metabolite) %>%
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~round(., 1)),
+                  mean_sd = glue::glue("{value_mean} ({value_sd})"),
+                  median_iqr = glue::glue("{value_median} ({value_iqr})")
+                  ) %>%
+    dplyr::select(Metabolite = metabolite,
+                  `mean (SD)` = mean_sd,
+                  `median (IQR)` = median_iqr)
 }
 
 #' Histogram faceted by metabolite
@@ -26,4 +32,15 @@ create_plot_distributions <- function(data) {
     ggplot2::geom_histogram() +
     ggplot2::facet_wrap(~metabolite, scales = "free") +
     ggplot2::theme_minimal()
+}
+
+#' Clean lipidomics data
+#'
+#' @param data Lipidomics data
+#'
+#' @returns a tibble
+clean <- function(data){
+  data %>%
+    dplyr::summarise(value = mean(value),
+                     .by = c(code, gender, age, class, metabolite))
 }
