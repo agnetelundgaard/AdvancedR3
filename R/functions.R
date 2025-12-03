de2c4eb8de7c8421dde9eeeb3b7edc775b157a70
Agnete Troen Lundgaard <agnete.troen.lundgaard@regionh.dk>
2025-12-03 11:00:57 +0100

    Save descriptive plot function in functions

--- R/functions.R
+++ R/functions.R
@@ -10,7 +10,20 @@ create_table_descriptive_stats <- function(data) {
   data %>%
     dplyr::summarise(mean = mean(value), sd = sd(value), .by = metabolite) %>%
     dplyr::mutate(dplyr::across(c(mean, sd), ~ round(., 1)),
-                  mean_sd = glue::glue("{mean} ({sd})")
+      mean_sd = glue::glue("{mean} ({sd})")
     ) %>%
     dplyr::select(Metabolite = metabolite, `mean (SD)` = mean_sd)
+}
+
+#' Histogram faceted by metabolite
+#'
+#' @param data Lipidomics data
+#'
+#' @returns A plot object
+create_plot_distributions <- function(data) {
+  data %>%
+    ggplot2::ggplot(ggplot2::aes(x = value)) +
+    ggplot2::geom_histogram() +
+    ggplot2::facet_wrap(~metabolite, scales = "free") +
+    ggplot2::theme_minimal()
 }
