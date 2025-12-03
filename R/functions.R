001616c36b87e62ac1585d8ea25f665a4cba6998
Agnete Troen Lundgaard <agnete.troen.lundgaard@regionh.dk>
2025-12-03 16:01:51 +0100

    Added regression model to functions

--- R/functions.R
+++ R/functions.R
@@ -8,17 +8,25 @@
 #' @return A tibble
 create_table_descriptive_stats <- function(data) {
   data %>%
-    dplyr::summarise(dplyr::across(value,
-                            list(mean = mean, sd = sd,
-                                 median = median, iqr = IQR)),
-                            .by = metabolite) %>%
-    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~round(., 1)),
-                  mean_sd = glue::glue("{value_mean} ({value_sd})"),
-                  median_iqr = glue::glue("{value_median} ({value_iqr})")
-                  ) %>%
-    dplyr::select(Metabolite = metabolite,
-                  `mean (SD)` = mean_sd,
-                  `median (IQR)` = median_iqr)
+    dplyr::summarise(
+      dplyr::across(
+        value,
+        list(
+          mean = mean, sd = sd,
+          median = median, iqr = IQR
+        )
+      ),
+      .by = metabolite
+    ) %>%
+    dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(., 1)),
+      mean_sd = glue::glue("{value_mean} ({value_sd})"),
+      median_iqr = glue::glue("{value_median} ({value_iqr})")
+    ) %>%
+    dplyr::select(
+      Metabolite = metabolite,
+      `mean (SD)` = mean_sd,
+      `median (IQR)` = median_iqr
+    )
 }
 
 #' Histogram faceted by metabolite
@@ -39,10 +47,12 @@ create_plot_distributions <- function(data) {
 #' @param data Lipidomics data
 #'
 #' @returns a tibble
-clean <- function(data){
+clean <- function(data) {
   data %>%
-    dplyr::summarise(value = mean(value),
-                     .by = c(code, gender, age, class, metabolite))
+    dplyr::summarise(
+      value = mean(value),
+      .by = c(code, gender, age, class, metabolite)
+    )
 }
 
 #' Preprocess by scaling
@@ -55,5 +65,24 @@ preprocess <- function(data) {
     dplyr::mutate(
       class = factor(class),
       value = scale(value)
+    )
+}
+
+#' Binomial mode
+#'
+#' @param data data frame with one metabolite
+#' @param model formula
+#'
+#' @returns model results in tibble
+fit_model <- function(data, model) {
+  glm(model,
+    data = data,
+    family = binomial
+  ) %>%
+    broom::tidy(exponentiate = TRUE) %>%
+    dplyr::mutate(.,
+      metabolite = unique(data$metabolite),
+      model = format(model),
+      .before = everything()
     )
 }
