7f7f493edb03aa97c5aef288025d888629a7c140
Agnete Troen Lundgaard <agnete.troen.lundgaard@regionh.dk>
2025-12-03 09:51:51 +0100

    Added descriptive table as a function

---
+++ R/functions.R
@@ -0,0 +1,13 @@
+#' Create descriptive table
+#'
+#' @param data
+create_table_descriptive_stats <- function(data) {
+  data |>
+    dplyr::summarise(mean = mean(value), sd = sd(value), .by = metabolite) |>
+    dplyr::mutate(dplyr::across(c(mean, sd), ~ round(., 1)),
+                  mean_sd = glue::glue("{mean} ({sd})")
+    ) |>
+    dplyr::select(Metabolite = metabolite, `mean (SD)` = mean_sd)
+}
+
+create_table_descriptive_stats(lipidomics)
